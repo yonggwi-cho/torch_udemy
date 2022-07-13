3bd76da37d29a03d47532ca61703fc2c2bb9800c
Yong-Gwi Cho <yonggwi.cho@sony.com>
2022-07-13 18:30:56 +0900

    write image_generation.py.

--- image_generation.py
+++ image_generation.py
@@ -6,6 +6,24 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
+from torch import optim
+
+class Net(nn.Module):
+    def __init__(self,n_in,n_mid,n_out) -> None:
+        super().__init__()
+        self.rnn = nn.LSTM(
+            input_size=n_in,
+            hidden_size=n_mid,
+            batch_first=True,
+        )
+        self.fc = nn.Linear(n_mid,n_out)
+
+    def forward(self,x):
+        y_rnn,(h,c) = self.rnn(x,None)
+        y = self.fc(y_rnn[:,-1,:])
+        return y
+    
 
 fmnist_data = FashionMNIST(root="./data",
                             train=True, download=True,
@@ -14,12 +32,24 @@ fmnist_classes = np.array(["T-shirt/top","Trouser","Pullover","Dress","Coat","Sa
 
 print("# data :", len(fmnist_data))
 
+n_epoch = 25
 n_image = 25
+img_size = 28
+n_time = 14
+n_disp = 10
+n_in = img_size
+n_mid = 256
+n_out = img_size
+n_sample_in_img = img_size-n_time
+
 fmnist_loader = DataLoader(fmnist_data,batch_size=n_image,shuffle=True)
 dataiter = iter(fmnist_loader)
 images,labels = dataiter.next()
 
-img_size = 28
+net = Net(n_in,n_mid,n_out)
+net.cuda(1)
+print(net)
+
 plt.figure(figsize=(10,10))
 for i in range(n_image):
     plt.subplot(5,5,i+1)
@@ -29,12 +59,6 @@ for i in range(n_image):
     plt.tick_params(labelbottom=False,labelleft=False,bottom=False,left=False)
 
 #plt.show()
-
-n_time = 14
-n_in = img_size
-n_mid = 256
-n_out = img_size
-n_sample_in_img = img_size-n_time
 
 dataloader = DataLoader(fmnist_data,batch_size=len(fmnist_data),shuffle=False)
 dataiter = iter(dataloader)
@@ -56,11 +80,63 @@ input_data = torch.tensor(input_data,dtype=torch.float)
 correct_data = torch.tensor(correct_data,dtype=torch.float)
 dataset = torch.utils.data.TensorDataset(input_data,correct_data)
 
+
+
+# train
 train_loader = DataLoader(dataset,batch_size=128,shuffle=True)
+loss_fnc = nn.MSELoss()
+optimizer = optim.Adam(net.parameters())
 
-n_disp = 10
+# validation
 disp_data = FashionMNIST(root="./data",
-                        )
+                        train=False, download=True,
+                        transform=transforms.ToTensor())
+disp_loader = DataLoader(disp_data,batch_size=n_disp,shuffle=False)
+dataiter = iter(disp_loader)
+disp_imgs,labels = dataiter.next()
+disp_imgs = disp_imgs.reshape(-1,img_size,img_size)
 
+def generate_images():
+    print("Original:")
+    plt.figure(figsize=(20,2))
+    for i in range(n_disp):
+        ax = plt.subplot(1,n_disp,i+1)
+        plt.imshow(disp_imgs[i],cmap="Gryes_r",vmin=0.0,vmax=1.0)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
+    print("Generated:")
+    gen_imgs = disp_imgs.clone()
+    for i in range(n_disp):
+        for j in range(n_sample_in_img):
+            x = gen_imgs[i,j:j+n_time].reshape(-1,n_time,img_size)
+            x = x.cuda(1)
+            gen_imgs[i,j+n_time] = net(x)[0]
+        ax = plt.subplot(1,n_disp,i+1)
+        plt.imshow(gen_imgs[i].detach(),cmap="Greys_r",vmin=0.0,vmax=1.0)
+        ax.get_xaxis().set_visibile(False)
+        ax.get_yaxis().set_visibile(False)
+
+hist_loss_train = list()
+hist_loss_test = list()
+
+for i in range(n_epoch):
+    net.train()
+    loss_train = 0
+    for j, (x,t) in enumerate(train_loader):
+        x,t = x.cuda(1),t.cuda(1)
+        y = net(x)
+        loss = loss_fnc(y,t)
+        loss_train += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    loss_train /= j+1
+    hist_loss_train.append(loss_train)
+
+    if i%1 == 0 :
+        print("Epoch:",i,"Loss_Train:",loss_train)
+        generate_images()
 
 
